c143eb91b185c6cab52d7d9d428364bb18879c9a
rasta4422 <massengill.ati@gmail.com>
2024-05-19 18:32:39 -0500

    clean up indenting

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -7,83 +7,83 @@
 
 
 static bool check(const char* str, size_t length) {
-	const unsigned char* u_str = (const unsigned char*) str;
-	for (size_t i = 0; i < length; i++){
+    const unsigned char* u_str = (const unsigned char*) str;
+    for (size_t i = 0; i < length; i++){
         term_print(u_str[i]);
-		if ((int)u_str[i] == EOF)
-			return false;
+        if ((int)u_str[i] == EOF)
+            return false;
     }
-	return true;
+    return true;
 }
 
 int printf(const char* restrict format, ...) {
-	va_list parameters;
-	va_start(parameters, format);
+    va_list parameters;
+    va_start(parameters, format);
 
-	int written = 0;
+    int written = 0;
 
-	while (*format != '\0') {
-		size_t max = INT_MAX - written;
-        
+    while (*format != '\0') {
+        size_t max = INT_MAX - written;
+
         /* check if each string is printable to VGA */
-		if (format[0] != '%' || format[1] == '%') {
-			/* escaping % */
+        if (format[0] != '%' || format[1] == '%') {
+            /* escaping % */
             if (format[0] == '%')
-				format++;
-			
+                format++;
+
             size_t idx = 1;
-			while (format[idx] && format[idx] != '%')
-				idx++;
+            while (format[idx] && format[idx] != '%')
+                idx++;
 
-			if (max < idx)
-				return -1;
+            if (max < idx)
+                return -1;
 
-			if (!check(format, idx))
-				return -1;
+            if (!check(format, idx))
+                return -1;
 
-			format += idx;
-			written += idx;
-			continue;
-		}
+            format += idx;
+            written += idx;
+            continue;
+        }
 
-		const char* format_begin = format++;
+        const char* format_begin = format++;
 
-		if (*format == 'c') {
-			format++;
-			char c = (char) va_arg(parameters, int);
-			if (!max)
-				return -1;
+        if (*format == 'c') {
+            format++;
+            char c = (char) va_arg(parameters, int);
+            if (!max)
+                return -1;
 
-			if (!check(&c, sizeof(c)))
-				return -1;
-			
+            if (!check(&c, sizeof(c)))
+                return -1;
+
             written++;
-		} else if (*format == 's') {
-			format++;
-			const char* str = va_arg(parameters, const char*);
-			size_t len = strlen(str);
-			if (max < len)
-				return -1;
-			
-			if (!check(str, len))
-				return -1;
+        } else if (*format == 's') {
+            format++;
+            const char* str = va_arg(parameters, const char*);
+            size_t len = strlen(str);
+            if (max < len)
+                return -1;
 
-			written += len;
-		} else {
-			format = format_begin;
-			size_t len = strlen(format);
-			if (max < len)
-				return -1;
+            if (!check(str, len))
+                return -1;
 
-			if (!check(format, len))
-				return -1;
-			
             written += len;
-			format += len;
-		}
-	}
+        } else {
+            format = format_begin;
+            size_t len = strlen(format);
+            if (max < len)
+                return -1;
 
-	va_end(parameters);
-	return written;
+            if (!check(format, len))
+                return -1;
+
+            written += len;
+            format += len;
+        }
+    }
+
+    va_end(parameters);
+    return written;
 }
 
