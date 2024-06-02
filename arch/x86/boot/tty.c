0ddcc8d35c862264cb9aa4af6505b70d41c1c8fb
rasta4422 <massengill.ati@gmail.com>
2024-06-02 18:24:04 -0500

    clean up indents

--- arch/x86/boot/tty.c
+++ arch/x86/boot/tty.c
@@ -1,73 +1,4 @@
 #include <tty.h>
-//#include <string.h>
-
-
-//unsigned int strlen(const char*);
-
-// enum for different vga colors
-/*
-enum vga_color {
-    VGA_COLOR_BLACK = 0,
-    VGA_COLOR_BLUE = 1,
-    VGA_COLOR_GREEN = 2,
-    VGA_COLOR_CYAN = 3,
-    VGA_COLOR_RED = 4,
-    VGA_COLOR_MAGENTA = 5,
-    VGA_COLOR_BROWN = 6,
-    VGA_COLOR_LIGHT_GREY = 7,
-    VGA_COLOR_DARK_GREY = 8,
-    VGA_COLOR_LIGHT_BLUE = 9,
-    VGA_COLOR_LIGHT_GREEN = 10,
-    VGA_COLOR_LIGHT_CYAN = 11,
-    VGA_COLOR_LIGHT_RED = 12,
-    VGA_COLOR_LIGHT_MAGENTA = 13,
-    VGA_COLOR_LIGHT_BROWN = 14,
-    VGA_COLOR_WHITE = 15,
-};
-
-// width and height of screen 
-static const unsigned int VGA_WIDTH = 80;
-static const unsigned int VGA_HEIGHT = 25;
-
-static unsigned int row;
-static unsigned int column;
-static unsigned char color;
-
-// VGA magic number 
-static unsigned short int *buffer = (unsigned short int*) 0xB8000;
-
-
-void tty_clear(void)
-{
-    row = 0;
-    column = 0;
-    color = VGA_COLOR_GREEN | VGA_COLOR_BLACK << 4;
-    for (unsigned int j = 0; j < VGA_HEIGHT; j++) {
-        for (unsigned int i = 0; i < VGA_WIDTH; i++) {
-            const unsigned int index = j * VGA_WIDTH + i;
-            buffer[index] = (unsigned short int) ' ' | (unsigned short int) color << 8;
-        }
-    }
-}
-
-void tty_print(const char *str)
-{
-    for (unsigned int i = 0; i < strlen(str); i++) {
-        const unsigned int idx = column * VGA_WIDTH + row;
-        buffer[idx] = (unsigned short int) str[i] | (unsigned short int) color << 8;
-        if (column == VGA_WIDTH - 1){
-            column = 0;
-            if (row == VGA_HEIGHT - 1){
-                row = 0;
-            }
-        }
-        column++;
-        row++;
-    }
-}
-
-*/
-
 typedef short int           int16_t;
 typedef unsigned char       uint8_t;
 typedef unsigned short int  uint16_t;
@@ -76,9 +7,9 @@ typedef unsigned long int   uint64_t;
 
 #include <bootboot.h>
 
-extern BOOTBOOT bootboot;               // 
-extern unsigned char environment[4096]; // configuration, UTF-8 text key=value pairs
-extern uint8_t fb;                      // linear framebuffer mapped
+extern BOOTBOOT bootboot; 
+extern unsigned char environment[4096];
+extern uint8_t fb;
 
 typedef struct {
     uint32_t magic;
